94e9981f0b265974586147fa2c5e3d1759eb7932
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-11-27 12:36:10 -0400

    Add aria-label to slider buttons

--- src/js/main.js
+++ src/js/main.js
@@ -23,7 +23,13 @@ import "../css/main.css";
         rewind: false,
         mode: "gallery",
         controlsText: ['<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>', '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>'],
+        controlsContainer: document.createElement("button").setAttribute("aria-label", "Slider buttons"),
         nav: false,
+        onInit: () => {
+          Array.from(document.querySelectorAll("[aria-controls='tns1']")).forEach((el) => {
+            el.setAttribute("aria-label", "Slider buttons");
+          });
+        },
       });
       slider.events.on("transitionEnd", () => {
         App.calcTimeAgo();
